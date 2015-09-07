e5f5937a0119395ad0a47e493ac616414bb02cb8
KlaytonH <klaytonhoffman@gmail.com>
2015-09-07 13:19:58 -0500

    Update Craps.java

--- Craps.java
+++ Craps.java
@@ -11,18 +11,20 @@ public class Craps {
 		
 		double percentage;
 		double rollcount;
+		//get the number of 2s, 3s, and 12s rolled in 752 instances.
 		rollcount = playCraps(752);
+		//calculate the percentage of 2s, 3s, and 12s rolled in 752 instances.
 		percentage = rollcount/752*100;
 		
-		System.out.println("The number of 2s,3s, and 12s rolled is: "+rollcount);
-		System.out.println("The percentage of 2s,3s, and 12s rolled is: "+percentage);
+		System.out.println("The number of 2s,3s, and 12s rolled is: "+rollcount +".");
+		System.out.println("The percentage of 2s,3s, and 12s rolled is: "+percentage + "%.");
 		
 	}
 	public static int playCraps(int numberOfRolls){
 		CrapsDice thedie = new CrapsDice();
 		int counter = 0;
 		int currentroll;
-		
+		//For each roll check if the sum of the dice = 2, 3, or 12 and increment counter if so
 		for(int i=1; i <=numberOfRolls; i++){
 			currentroll = thedie.roll();
 			if (currentroll == 2 || currentroll == 3 || currentroll == 12) {
